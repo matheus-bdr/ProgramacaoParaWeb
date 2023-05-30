c15b45af6bf31deef26790840d4245169d828bf0
matheus-bdr <mmatheuscruzs@gmail.com>
2023-05-29 23:23:36 -0300

    commit

---
+++ Desafio_Consulta_medica/index.js
@@ -0,0 +1,29 @@
+const express  = require('express')
+const mustacheExpress = require('mustache-express')
+
+const app = express()
+
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+app.use(express.urlencoded({extended: true}))
+
+const PORT = 8000
+app.listen(PORT, function () {
+    console.log('app rodando na porta ' + PORT)
+})
+
+app.get('/', function (req, res) {
+    res.render('index.html')
+})
+
+app.post('/mostra_dados', function (req, res) {
+    let dados_form = {
+        nome: req.body.name,
+        email: req.body.gmail,
+        telefone: req.body.tel,
+        msg: req.body.mensagem
+    }
+    console.log(dados_form)
+    res.render('apresentaDados.html', {dados_form})
+})
